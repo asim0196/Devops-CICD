c0c0c72e5b6f7ec9cf283bba696306490e8ad4c2
asim0196 <112340466+asim0196@users.noreply.github.com>
2023-10-19 19:54:31 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "WELCOME TO DEVOPS SHACK";
+		return "WELCOME TO world";
 	}
 
 	public static void main(String[] args) {
