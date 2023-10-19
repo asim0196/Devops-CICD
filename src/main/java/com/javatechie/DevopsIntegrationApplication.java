367fdb6669aab5dba2eea5fbf923eb50f5d21a55
asim0196 <112340466+asim0196@users.noreply.github.com>
2023-10-19 20:07:25 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "WELCOME TO World";
+		return "WELCOME TO CloudBlitz";
 	}
 
 	public static void main(String[] args) {
